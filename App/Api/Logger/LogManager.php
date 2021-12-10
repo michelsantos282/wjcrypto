fc1a2507000a4fde414f3ef253d6ef25c0e69cff
Michel Santos <michel.santos@webjump.com.br>
2021-12-10 12:12:01 -0300

    fix views

--- App/Api/Logger/LogManager.php
+++ App/Api/Logger/LogManager.php
@@ -2,7 +2,44 @@
 
 namespace App\Api\Logger;
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
 class LogManager
 {
+    private static Logger $logger;
 
+    public function __construct(Logger $logger)
+    {
+        self::$logger = $logger;
+    }
+
+    public function log($message, $mode = 'info')
+    {
+        self::$logger->pushHandler(new StreamHandler(__DIR__ . '/logs.txt'));
+
+        switch ($mode) {
+            case 'info':
+                self::$logger->info($message);
+                break;
+            case 'warning':
+                self::$logger->warning($message);
+                break;
+            case 'error':
+                self::$logger->error($message);
+                break;
+            case 'debug':
+                self::$logger->debug($message);
+                break;
+            case 'critical':
+                self::$logger->critical($message);
+                break;
+            case 'alert':
+                self::$logger->alert($message);
+                break;
+            case 'emergency':
+                self::$logger->emergency($message);
+                break;
+        }
+    }
 }
